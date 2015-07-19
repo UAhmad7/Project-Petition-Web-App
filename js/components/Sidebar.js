b7211764f09517037cc1b830dc51205b4603522e
Umair Ahmad <uahmad7@hotmail.com>
2015-07-18 23:14:01 -0400

    New web pages

---
+++ js/components/Sidebar.js
@@ -0,0 +1,28 @@
+var Sidebar = React.createClass({
+
+	render: function() {
+		return (
+			<div className="ui vertical inverted sticky menu fixed top nav-sidebar">
+				<a className="item" href="index.html">
+					<i className="home icon"></i>
+					Home
+				</a>
+				<a className="item" href="activity.html">
+					<i className="file text icon"></i>
+					Petitioning Activity
+				</a>
+				<a className="item" href="graphs.html">
+					<i className="pie graph icon"></i>
+					Data Graphs
+				</a>
+				<a className="item" href="about.html">
+					<i className="info circle icon"></i>
+					About
+				</a>
+			</div>
+		);
+	}
+
+});
+
+var navigationSidebar = React.render(<Sidebar />, document.getElementById('navigation-menu'));
